cd00cae061c0ec2f9425dd561cde0762bbff7eb9
Jaroslaw Tworek <jerry@millionintegrals.com>
2013-07-08 11:25:34 +0200

    Implemented state mutation using lenses

--- Main.hs
+++ Main.hs
@@ -43,7 +43,6 @@ mainLoop = do
   processEvents
   exit <- use shouldExit
   unless exit mainLoop
-  --if exit then return () else mainLoop
 
 render :: GameMonad ()
 render = do
@@ -51,6 +50,7 @@ render = do
   liftIO $ do
     GL.clear [GL.ColorBuffer]
     GL.loadIdentity
+    -- This is a mongolian part to be changed later
     GLU.lookAt (toVert3 $ _pos state) (toVert3 $ center state) (toVec3 $ _up state)
 
     GL.renderPrimitive GL.Triangles $ do
@@ -106,49 +106,49 @@ render = do
 setPolygonMode :: Bool -> IO ()
 setPolygonMode flag = GL.polygonMode $= (if flag then (GL.Line, GL.Line) else (GL.Fill, GL.Fill))
 
---processSpace :: GameState -> IO GameState
---processSpace state = do
---  space <- GLFW.getKey ' '
---  if space == GLFW.Press then do
---    if wasPressed state then return state else do
---      let newState = state { wasPressed = True, stateLine = not (stateLine state) }
---      setPolygonMode $ stateLine newState
---      return newState
---  else if wasPressed state then return $ state { wasPressed = False } else return state
+processSpace :: GameMonad ()
+processSpace = do
+  space <- liftIO $ GLFW.getKey ' '
+  pressed <- use wasPressed
+
+  if space == GLFW.Press then do
+    unless pressed $ do
+      wasPressed .= True
+      stateLine %= not
+      liftIO . setPolygonMode =<< use stateLine
+  else when pressed $ wasPressed .= False
 
 processEscape :: GameMonad ()
 processEscape = do
   p <- liftIO $ GLFW.getKey GLFW.ESC
-  if p == GLFW.Press then shouldExit .= True else return ()
+  when (p == GLFW.Press) $ shouldExit .= True
 
---processKey :: Char -> (GameState -> GameState) -> GameState -> IO GameState
---processKey key action state = do
---  val <- GLFW.getKey key
---  if val == GLFW.Press then return $ action state else return state
-
---stateCombinator :: [GameState -> IO GameState] -> GameState -> IO GameState
---stateCombinator = flip $ foldr (=<<) . return
+processKey :: Char -> GameMonad () -> GameMonad ()
+processKey key action = (liftM (==GLFW.Press) $ liftIO $ GLFW.getKey key) >>= flip when action
 
 stateCombinator :: [GameMonad a] -> GameMonad ()
 stateCombinator = foldr (>>) $ return ()
 
---processMove :: GameState -> IO GameState
---processMove state = stateCombinator [processA, processD, processW, processS] state
---  where crx = L.cross (direction state) (up state)
---        moveSpeed = 0.05
---        processA = processKey 'A' (\s -> s { pos = (pos s) + (-moveSpeed) * crx })
---        processD = processKey 'D' (\s -> s { pos = (pos s) + moveSpeed * crx })
---        processW = processKey 'W' (\s -> s { pos = (pos s) + moveSpeed * (direction state) })
---        processS = processKey 'S' (\s -> s { pos = (pos s) + (-moveSpeed) * (direction state) })
---
---processRotation :: GameState -> IO GameState
---processRotation = stateCombinator [processQ, processE]
---  where phi = pi/360
---        processQ = processKey 'Q' (\s -> s { direction = rotateXZ phi $ direction s })
---        processE = processKey 'E' (\s -> s { direction = rotateXZ (-phi) $ direction s }) 
+processMove :: GameMonad ()
+processMove = do
+  dirvec <- use direction
+  upvec <- use up
+  let crx = L.cross dirvec upvec
+  let moveSpeed = 0.05
+  let processA = processKey 'A' $ pos -= moveSpeed * crx
+  let processD = processKey 'D' $ pos += moveSpeed * crx
+  let processW = processKey 'W' $ pos += moveSpeed * dirvec
+  let processS = processKey 'S' $ pos -= moveSpeed * dirvec
+  stateCombinator [processA, processD, processW, processS]
+
+processRotation :: GameMonad ()
+processRotation = stateCombinator [processQ, processE]
+  where phi = pi/360
+        processQ = processKey 'Q' $ direction %= rotateXZ phi
+        processE = processKey 'E' $ direction %= rotateXZ (-phi)
 
 processEvents :: GameMonad ()
-processEvents = stateCombinator [processEscape]
+processEvents = stateCombinator [processEscape, processSpace, processMove, processRotation]
 
 vertex3 :: GL.GLfloat -> GL.GLfloat -> GL.GLfloat -> GL.Vertex3 GL.GLfloat
 vertex3 = GL.Vertex3
