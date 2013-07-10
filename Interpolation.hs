3866eca19510cc58001c30f0c8c0cc9105313767
michal_garmulewicz <garmulewicz@gmail.com>
2013-07-10 09:25:09 +0200

    needed fix for one thing

--- Interpolation.hs
+++ Interpolation.hs
@@ -1,5 +1,10 @@
+interpolate :: [Float] -> [Float]
+interpolate (x:y:[]) = [x, (x+y)/2, y]
+interpolate (x:y:xs) = [x, (x+y)/2] ++ interpolate (y:xs)
+interpolate x = x
+
 interpM :: [[Float]] -> [[Float]]
-interpM x = interpCols ( interpRows x)
+interpM x = interpCol ( interpRow x)
 
 interpRow :: [[Float]] -> [[Float]]
 interpRow x = [ interpolate a|a<-x]
@@ -7,4 +12,4 @@ interpRow x = [ interpolate a|a<-x]
 interpCol :: [[Float]] -> [[Float]]
 interpCol [] = []
 interpCol (x:[]) = [x]
-interpCol x = [head x] ++ [zipWith ((\ a b -> (a+b)/2)) (head x) (head (tail x))] ++ interpCols( tail x)
+interpCol x = [head x] ++ [zipWith ((\ a b -> (a+b)/2)) (head x) (head (tail x))] ++ interpCol( tail x)
